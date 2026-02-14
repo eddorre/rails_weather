0934fe48a128e68ab03f2bbc6f8f959f8565300a
Carlos Rodriguez <carlos@eddorre.com>
2026-02-14 01:12:44 -0800

    Add helper methods to translate weather codes and conditions to icons Also includes specs for the helper.

--- app/helpers/forecasts_helper.rb
+++ app/helpers/forecasts_helper.rb
@@ -1,2 +1,48 @@
 module ForecastsHelper
+  # Based on WMO Weather Interpretation Codes
+  # from https://open-meteo.com/en/docs
+  WEATHER_LABELS = {
+    0 => "Clear sky",
+    1 => "Mainly clear",
+    2 => "Partly cloudy",
+    3 => "Overcast",
+    45 => "Fog",
+    48 => "Fog",
+    51 => "Light drizzle",
+    53 => "Moderate drizzle",
+    55 => "Dense drizzle",
+    61 => "Light rain",
+    63 => "Moderate rain",
+    65 => "Heavy rain",
+    71 => "Light snow",
+    73 => "Moderate snow",
+    75 => "Heavy snow",
+    80 => "Rain showers",
+    95 => "Thunderstorm"
+  }.freeze
+
+  def weather_label(code)
+    WEATHER_LABELS[code] || "Unknown"
+  end
+
+    def weather_icon_key(code)
+    case code
+    when 0
+      "clear-day"
+    when 1, 2
+      "partly-cloudy-day"
+    when 3
+      "cloudy"
+    when 45, 48
+      "fog"
+    when 51..67, 80..82
+      "rain"
+    when 71..77
+      "snow"
+    when 95..99
+      "thunderstorms"
+    else
+      "cloudy"
+    end
+  end
 end
--- spec/helpers/forecasts_helper_spec.rb
+++ spec/helpers/forecasts_helper_spec.rb
@@ -1,15 +1,123 @@
-require 'rails_helper'
+require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the ForecastsHelper. For example:
-#
-# describe ForecastsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ForecastsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#weather_label" do
+    context "with known weather codes" do
+      it "returns 'Clear sky' for code 0" do
+        expect(helper.weather_label(0)).to eq("Clear sky")
+      end
+
+      it "returns 'Mainly clear' for code 1" do
+        expect(helper.weather_label(1)).to eq("Mainly clear")
+      end
+
+      it "returns 'Partly cloudy' for code 2" do
+        expect(helper.weather_label(2)).to eq("Partly cloudy")
+      end
+
+      it "returns 'Overcast' for code 3" do
+        expect(helper.weather_label(3)).to eq("Overcast")
+      end
+
+      it "returns 'Fog' for code 45" do
+        expect(helper.weather_label(45)).to eq("Fog")
+      end
+
+      it "returns 'Light drizzle' for code 51" do
+        expect(helper.weather_label(51)).to eq("Light drizzle")
+      end
+
+      it "returns 'Moderate rain' for code 63" do
+        expect(helper.weather_label(63)).to eq("Moderate rain")
+      end
+
+      it "returns 'Heavy snow' for code 75" do
+        expect(helper.weather_label(75)).to eq("Heavy snow")
+      end
+
+      it "returns 'Thunderstorm' for code 95" do
+        expect(helper.weather_label(95)).to eq("Thunderstorm")
+      end
+    end
+
+    context "with unknown weather codes" do
+      it "returns 'Unknown' for unrecognized codes" do
+        expect(helper.weather_label(999)).to eq("Unknown")
+      end
+    end
+  end
+
+  describe "#weather_icon_key" do
+    context "with clear weather" do
+      it "returns 'clear-day' for code 0" do
+        expect(helper.weather_icon_key(0)).to eq("clear-day")
+      end
+    end
+
+    context "with partly cloudy weather" do
+      it "returns 'partly-cloudy-day' for code 1" do
+        expect(helper.weather_icon_key(1)).to eq("partly-cloudy-day")
+      end
+
+      it "returns 'partly-cloudy-day' for code 2" do
+        expect(helper.weather_icon_key(2)).to eq("partly-cloudy-day")
+      end
+    end
+
+    context "with overcast weather" do
+      it "returns 'cloudy' for code 3" do
+        expect(helper.weather_icon_key(3)).to eq("cloudy")
+      end
+    end
+
+    context "with fog" do
+      it "returns 'fog' for code 45" do
+        expect(helper.weather_icon_key(45)).to eq("fog")
+      end
+
+      it "returns 'fog' for code 48" do
+        expect(helper.weather_icon_key(48)).to eq("fog")
+      end
+    end
+
+    context "with rain" do
+      it "returns 'rain' for code 51 (light drizzle)" do
+        expect(helper.weather_icon_key(51)).to eq("rain")
+      end
+
+      it "returns 'rain' for code 63 (moderate rain)" do
+        expect(helper.weather_icon_key(63)).to eq("rain")
+      end
+
+      it "returns 'rain' for code 80 (rain showers)" do
+        expect(helper.weather_icon_key(80)).to eq("rain")
+      end
+    end
+
+    context "with snow" do
+      it "returns 'snow' for code 71 (light snow)" do
+        expect(helper.weather_icon_key(71)).to eq("snow")
+      end
+
+      it "returns 'snow' for code 75 (heavy snow)" do
+        expect(helper.weather_icon_key(75)).to eq("snow")
+      end
+    end
+
+    context "with thunderstorm" do
+      it "returns 'thunderstorms' for code 95" do
+        expect(helper.weather_icon_key(95)).to eq("thunderstorms")
+      end
+
+      it "returns 'thunderstorms' for code 99" do
+        expect(helper.weather_icon_key(99)).to eq("thunderstorms")
+      end
+    end
+
+    context "with unknown codes" do
+      it "returns 'cloudy' as the default" do
+        expect(helper.weather_icon_key(999)).to eq("cloudy")
+      end
+    end
+  end
 end
